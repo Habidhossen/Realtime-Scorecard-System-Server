df223b5742531054c036555c81d57ba780098dd1
Habidhossen <habidhossen2@gmail.com>
2023-02-04 21:39:17 +0600

    setup the server

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const port = process.env.PORT || 4000;
-
 const app = express();
+
+// MIDDLEWARE
 app.use(express.json());
 app.use(cors());
 
