b88ced69ab8c54e554d95b6844ce4578eb0470c9
Habidhossen <habidhossen2@gmail.com>
2023-02-04 21:37:37 +0600

    setup the server

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT || 4000;
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// HOME ROUTE
+app.get("/", (req, res) => {
+  res.status(200).send("Realtime Scorecard server is running");
+});
+
+// LISTENING SERVER
+app.listen(port, () => {
+  console.log(`listening on port: ${port}`);
+});
+
+// IF ROUTE NOT FOUND
+app.use((req, res, next) => {
+  res.status(404).json({
+    message: "Route Not Found",
+  });
+});
+
+// IF SERVER ERROR
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    message: "Something broke",
+  });
+});
